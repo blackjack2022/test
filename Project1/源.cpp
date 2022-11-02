1d4ffeed03a8c845baaabe125d3241f0ec2cd833
blackjack2022 <3141903698@qq.com>
2022-11-02 22:08:58 +0800

    hello

---
+++ Project1/源.cpp
@@ -0,0 +1,17 @@
+
+#include<bits/stdc++.h>
+#define IOS	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define endl "\n"
+#define ll long long
+#define INF 0x3f3f3f
+const ll N = 1e6 + 10;
+using namespace std;
+//素数判断 primejudge
+//高精度 + - * /
+//质因数分解 pf
+//最大公因数maxdiv
+int main()
+{
+	IOS;
+	return 0;
+}
